5630ac8354f843dabf2daa765aa8d90ceeddf5d7
mexus <gilaldpellaeon@gmail.com>
2020-10-16 00:28:19 +0300

    Moar windows

--- src/processing.rs
+++ src/processing.rs
@@ -1,6 +1,5 @@
 //! Processing helpers.
 
-use itertools::Itertools;
 use ndarray::Array2;
 use rustfft::{num_complex::Complex, num_traits::Zero, FFTplanner};
 use std::f32::consts::PI;
@@ -46,8 +45,8 @@ impl<AnyWindow, AnySamples> ProcessorBuilder<AnyWindow, AnySamples> {
     {
         assert!(!window.as_ref().is_empty(), "Window must be non-empty");
         assert!(
-            window.as_ref().len() % 2 == 0,
-            "Window size must be divisible by two"
+            window.as_ref().len() % 2 == 1,
+            "Window size must NOT be divisible by two"
         );
         ProcessorBuilder {
             window,
@@ -68,9 +67,34 @@ where
 {
     /// Processes the given audio samples using a short-time discrete
     /// fast-fourier transform.
-    pub fn process(&self) -> Array2<f32> {
+    ///
+    /// Single window at time `t`:
+    ///         t
+    ///         -
+    ///        / \
+    ///       /   \
+    ///      /     \
+    /// t-w/2  ...  t+w/2
+    ///
+    /// time from 0 to 9 (length = 10), windows of size 7 (w/2 = 3):
+    /// 1. `t` from -3 to 3 (top at `t` = 0)
+    /// 2. `t` from 0 to 6  (top at `t` = 3)
+    /// 3. `t` from 3 to 9  (top at `t` = 6)
+    /// 4. `t` from 6 to 12 (top at `t` = 9)
+    ///
+    /// t =     0  3  6  9
+    ///         -  -  -  -   
+    ///        / \/ \/ \/ \  
+    ///       /  /\ /\ /\  \
+    ///      /  /  X  X  \  \
+    /// t = -3  0  3  6  9  12
+    ///
+    /// Base points count = length / (w/2) + 1 = 10 / 3 + 1 = 3 + 1 = 4
+    pub fn process(&self) -> ProcessingResult {
         let window = self.window.as_ref();
         let window_size = window.len();
+
+        let half_width = window_size / 2;
 
         let samples = self.samples.as_ref();
         let samples_count = samples.len();
@@ -78,40 +102,58 @@ where
         let mut temp_input: Vec<Complex<f32>> = vec![Zero::zero(); window_size];
         let mut temp_output: Vec<Complex<f32>> = vec![Zero::zero(); window_size];
 
-        let mut heat_map = Array2::zeros((samples_count / window_size, samples_count / 2));
+        let mut heat_map = Array2::zeros((samples_count / half_width + 1, samples_count / 2));
 
         let mut planner = FFTplanner::new(false);
         let fft = planner.plan_fft(window_size);
 
-        for (samples, heat_row) in (0..)
-            .step_by(window_size / 2)
-            .map(|time_shift| {
-                let samples = samples.get(time_shift..)?;
-                samples.get(..window_size)
-            })
-            .while_some()
-            .zip(heat_map.genrows_mut())
-        {
-            temp_input.iter_mut().zip(samples).zip(window).for_each(
-                |((input, &sample), &window_coefficient)| {
-                    *input = Complex::new(sample as f32 * window_coefficient, 0.)
-                },
-            );
+        let mut minimum_value = f32::MAX;
+        let mut maximum_value = f32::MIN;
+
+        for (index, heat_row) in heat_map.genrows_mut().into_iter().enumerate() {
+            let frame_begin = index * half_width - half_width;
+            let frame_end = index * half_width + half_width;
+
+            dbg!(frame_begin..=frame_end)
+                .zip(&mut temp_input)
+                .zip(window)
+                .for_each(|((t, input), &window_coefficient)| {
+                    *input = Complex::new(
+                        samples
+                            .get(t)
+                            .map(|&value| value as f32 * window_coefficient)
+                            .unwrap_or(0.),
+                        0.,
+                    );
+                });
+
             fft.process(&mut temp_input, &mut temp_output);
 
             let heat_row: &mut [f32] = heat_row.into_slice().expect("Contiguous array");
-            debug_assert_eq!(heat_row.len(), samples_count / 2);
-
             heat_row
                 .iter_mut()
                 .zip(&temp_output)
                 .for_each(|(output, temp_output)| {
-                    *output = temp_output.norm_sqr() / window_size as f32;
+                    let value = temp_output.norm_sqr() / window_size as f32;
+                    minimum_value = minimum_value.min(value);
+                    maximum_value = maximum_value.max(value);
+                    *output = value;
                 });
         }
 
-        heat_map
+        ProcessingResult {
+            heat_map,
+            minimum_value,
+            maximum_value,
+        }
     }
+}
+
+/// Processing result.
+pub struct ProcessingResult {
+    pub heat_map: Array2<f32>,
+    pub minimum_value: f32,
+    pub maximum_value: f32,
 }
 
 /// Hann window.
@@ -138,8 +180,8 @@ impl HannWindow {
 }
 
 #[cfg(test)]
-mod test {
-    use super::*;
+mod hann_window_test {
+    use super::HannWindow;
 
     #[test]
     fn check_zero() {
@@ -147,6 +189,49 @@ mod test {
             let window = HannWindow::new(length);
             assert!(window.coefficient(0) < std::f32::MIN_POSITIVE);
             assert!(window.coefficient(length - 1) < std::f32::MIN_POSITIVE);
+        }
+    }
+
+    #[test]
+    fn check_symmetry() {
+        for length in 2..1024 {
+            if length % 2 == 0 {
+                // If length % 2 == 0, there is no central point.
+                continue;
+            }
+            let window = HannWindow::new(length);
+            for i in 0..length / 2 {
+                let left = window.coefficient(length / 2 - i);
+                let right = window.coefficient(length / 2 + i);
+                let diff = left - right;
+                assert!(
+                    diff.abs() < std::f32::MIN_POSITIVE,
+                    "f({}) - f({}) = {}",
+                    length / 2 - i,
+                    length / 2 + i,
+                    diff
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn check_peak() {
+        for length in 2..1024 {
+            if length % 2 == 0 {
+                // If length % 2 == 0, there is no central point.
+                continue;
+            }
+            let window = HannWindow::new(length);
+
+            // Peak must be reached at the central point.
+            let expected = window.coefficient(length / 2);
+
+            // Test that all the other points contain lesser values.
+            let less = (0..length)
+                .filter(|&idx| idx != length / 2)
+                .all(|idx| window.coefficient(idx) < expected);
+            assert!(less, "length = {}", length);
         }
     }
 }
